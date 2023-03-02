0efc633a48f0e165802995ff20b4c2c990ed4807
Gustavo Guarany <gugaguarany@gmail.com>
2023-03-02 17:25:11 -0300

    Adicinando alguns recursos

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,29 @@ print("Bem vindo ao jogo de adivinhação!!")
 print("**********************************")
 
 numero_secreto = 42
+numero_tentativas = 4
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while (rodada < numero_tentativas):
 
-print ("Você digitou o numero: ", chute_str)
+    chute_str = input("Digite o seu numero: ")
+    print ("Você digitou o numero: ", chute_str)
+    print ("Numero de tentativas:",rodada ,"de" , numero_tentativas)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-if (numero_secreto == chute):
-    print ("Você acertou")
-else:
-    print ("Você errou")
+
+    if (acertou):
+        print ("Você acertou")
+    else:
+        if(maior):
+            print("Você chutou um numero maior!")
+        elif(menor):
+            print ("Você chutou um numero menor!")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
