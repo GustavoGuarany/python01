e106208a9c3960c70ac5b2febbce55b1ed6d577c
Gustavo Guarany <gugaguarany@gmail.com>
2023-03-07 15:21:34 -0300

    Criando jogo forca

---
+++ forca.py
@@ -0,0 +1,11 @@
+def jogar():
+
+    print("**********************************")
+    print("***Bem vindo ao jogo de Forca!!***")
+    print("**********************************")
+
+    palavra_secreta = "banana"
+
+
+if (__name__ == "__main__"):
+    jogar()
