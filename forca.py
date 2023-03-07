f51de1a605d0e0c015670a63452987a0b414c58b
Gustavo Guarany <gugaguarany@gmail.com>
2023-03-07 16:06:44 -0300

    01 modulo - projeto jogo de forca  aula 1

--- forca.py
+++ forca.py
@@ -6,6 +6,23 @@ def jogar():
 
     palavra_secreta = "banana"
 
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+
+        chute = input("Qual a letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index + 1
+
+        print("Jogando...")
+
+    print("Fim do jogo")
+
 
 if (__name__ == "__main__"):
     jogar()
